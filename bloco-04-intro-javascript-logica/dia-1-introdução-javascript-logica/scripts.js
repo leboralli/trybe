41df4c6add79d559e978e8c27930adc891d9f2c1
Leandro Boralli <leboralli@gmail.com>
2021-08-25 18:09:08 -0300

    scripts iniciais do dia 4.1

---
+++ bloco-04-intro-javascript-logica/dia-1-introdução-javascript-logica/scripts.js
@@ -0,0 +1,10 @@
+const candidata = "aprovada";
+
+switch (candidata) {
+  case "aprovada":
+    console.log("Pessoa aprovada")
+    break;
+  case "reprovada":
+    console.log("Pessoa reprovada")
+    break;
+}
